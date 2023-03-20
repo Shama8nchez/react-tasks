6561658d650d91e1d270a3c356341a715811fe23
Shama8nchez <shamanouski@gmail.com>
2023-03-20 22:17:24 +0300

    refactor: constants

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,15 +3,16 @@ import About from "./components/pages/About/About";
 import Notfound from "./components/pages/Notfound/404";
 import Main from "./components/pages/Main/main";
 import Header from "./components/Header/Header";
+import { EPATH } from "data/constants";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Notfound />} />
+        <Route path={EPATH.MAIN} element={<Main />} />
+        <Route path={EPATH.ABOUT} element={<About />} />
+        <Route path={EPATH.NOTFOUND} element={<Notfound />} />
       </Routes>
     </>
   );
--- react-app/src/components/Header/Header.tsx
+++ react-app/src/components/Header/Header.tsx
@@ -1,19 +1,13 @@
 import React, { MouseEvent } from "react";
 import NavLi from "./NavLi";
 import HeaderTitle from "./HeaderTitle";
+import { EPAGES, EPATH } from "data/constants";
 
 function setTitle() {
-  if (
-    location.pathname === "/" ||
-    location.pathname === ""
-  )
-    return "Main";
-  else if (
-    location.pathname === "/about/" ||
-    location.pathname === "/about"
-  )
-    return "About us";
-  else return "404. Page not found";
+  if (location.pathname === "/" || location.pathname === "") return EPAGES.MAIN;
+  else if (location.pathname === "/about/" || location.pathname === "/about")
+    return EPAGES.ABOUT;
+  else return EPAGES.NOTFOUND;
 }
 
 class Header extends React.Component<Record<string, never>, { title: string }> {
@@ -37,13 +31,13 @@ class Header extends React.Component<Record<string, never>, { title: string }> {
         <nav>
           <ul className="nav__list">
             <NavLi
-              to={"/"}
-              title={"Main"}
+              to={EPATH.MAIN}
+              title={EPAGES.MAIN}
               onClick={this.handleClick}
             />
             <NavLi
-              to={"/about"}
-              title={"About us"}
+              to={EPATH.ABOUT}
+              title={EPAGES.ABOUT}
               onClick={this.handleClick}
             />
           </ul>
