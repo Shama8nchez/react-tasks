abbaf03001222dcce82c00d3339c55cf547ab68f
Shama8nchez <shamanouski@gmail.com>
2023-04-07 09:46:41 +0300

    feat: add loader

--- react-app/src/components/pages/Main/card.tsx
+++ react-app/src/components/pages/Main/card.tsx
@@ -3,22 +3,30 @@ import { TCard } from "../../../types";
 export function Card(props: { card: TCard }) {
   return (
     <div className="card">
-      <p className="card__title">{props.card.name}</p>
       <img src={props.card.image} alt="book" className="card__img" />
-      <p className="card__author">{props.card.status}</p>
-      <p className="card__price">{props.card.species}</p>
+      <h3 className="card__title">{props.card.name}</h3>
+      <p className="card__species">Species: {props.card.species}</p>
+      <p className="card__status">Status: {props.card.status}</p>
     </div>
   );
 }
 
-function Cards(props: { card: TCard[] }) {
-  return (
-    <div className="cards">
-      {props.card.map((item) => (
-        <Card card={item} key={item.id} />
-      ))}
-    </div>
-  );
+function Cards(props: { card: TCard[], str: string }) {
+  if (props.card.length === 0) {
+    return (
+      <div className="cards">
+        <p className="cards-nofound">{props.str}</p>
+      </div>
+    )
+  } else {
+    return (
+      <div className="cards">
+        {props.card.map((item) => (
+          <Card card={item} key={item.id} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Cards;
--- react-app/src/components/pages/Main/main.tsx
+++ react-app/src/components/pages/Main/main.tsx
@@ -6,19 +6,24 @@ import { TData } from "../../../types";
 
 function Main() {
   const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState("");
 
   const onSubmitSearch = (data: TData): void => {
+    setResult([]);
+    setLoading("Wait...");
     fetch(`${BASE_PATH}${SEARCH_PATH}${SEARCH_PARAM}=${data.search}`)
       .then((rez) => rez.json())
       .then((rez) => {
-        setResult(rez.results);
+        if (rez.results) {
+          setResult(rez.results);
+        } else setLoading("No result were found with this query");
       });
   };
 
   return (
     <main>
       <SearchBar func={onSubmitSearch} />
-      <Cards card={result} />
+      <Cards card={result} str={loading}/>
     </main>
   );
 }
